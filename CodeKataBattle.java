ce0d065ce2c6a64f23d035401aa5bc430b9751ea
codekatabattlese <157956352+codekatabattlese@users.noreply.github.com>
2024-02-14 09:15:01 +0100

    Add CodeKataBattle.java

---
+++ CodeKataBattle.java
@@ -0,0 +1,13 @@
+package com.codekatabattle.codebattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebattleApplication.class, args);
+	}
+
+}
